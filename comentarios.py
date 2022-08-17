9ecbe69b95d96a6986192ace1fdc0a1fa45cfe24
JesusEmmanuelRodriguezUrdiales <jesuseru2003@gmail.com>
2022-08-16 20:22:24 -0500

    Comentarios

---
+++ comentarios.py
@@ -0,0 +1,14 @@
+# Esto es un ocmentario
+
+"""
+Esto es
+un comentario
+de más de una línea
+"""
+
+a = 2
+b = 4 # Esto es una suma de 2 números
+
+resultado = a + b
+
+print("La suma es:", resultado)
